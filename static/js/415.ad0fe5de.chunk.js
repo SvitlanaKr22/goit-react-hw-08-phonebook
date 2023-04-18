aaa3efe6f45c9ede259e4611cef7e41e3619e02c
SvitlanaKr22 <109691614+SvitlanaKr22@users.noreply.github.com>
2023-04-18 13:40:54 +0000

    Deploying to gh-pages from @ SvitlanaKr22/goit-react-hw-08-phonebook@d2b1b15dfabe703779a1b5ac0e725de99e5e9498 🚀

---
+++ static/js/415.ad0fe5de.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[415],{5415:function(e,n,o){o.r(n),o.d(n,{default:function(){return i}});var r=o(184);function i(){return(0,r.jsx)("div",{children:(0,r.jsxs)("h1",{children:["Phonebook"," ",(0,r.jsx)("span",{role:"img","aria-label":"Greeting icon",children:"\ud83d\udc81\ud83d\udd6e"})]})})}}}]);
+//# sourceMappingURL=415.ad0fe5de.chunk.js.map
